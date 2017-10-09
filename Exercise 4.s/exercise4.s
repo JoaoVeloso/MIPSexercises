47605be5e62475bba670453ea3a1e93bcc65328c
2161797@my.ipleiria.pt <2161797@my.ipleiria.pt>
2017-10-09 17:06:57 +0100

    Falta Acabar

---
+++ Exercise 4.s/exercise4.s
@@ -0,0 +1,14 @@
+        .data
+CR:     .word32 0x10000 ; Control Register tem o endereço 10000
+DR:     .word32 0x10008 ; Data Register tem o endereço 10008
+A:      .word 10
+B:      .word 8
+Z:      .word 2
+C:      .word 0
+
+        .text
+main:
+        ;Exercicio4
+        ;Adapte o exercício 3 de forma a obter do utilizador os valores a somar
+
+        ;Falta Acabar
